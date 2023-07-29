5d32fcab7115daeced58d582b477149208843364
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-07-29 20:28:33 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "decorlib",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Richard Antal Nagy",
     author_email="nagy.richard.antal@gmail.com",
     description="Python library with decorators for ease of development",
